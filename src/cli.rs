b8d53ebaf037ebf80690e3f148e9f6bc4e7d6d83
dthompson <dthompson@edg.io>
2024-04-12 09:42:54 -0400

    fix version discrep

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[clap(name = "my-notes", version = "1.2.1", about = "A simple note taking app")]
+#[clap(name = "my-notes", version = "1.2.2", about = "A simple note taking app")]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
